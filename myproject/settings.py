4825c0900856a1994232fc910cc7c0c5ccd0c784
yash5015 <yashrathor5015@gmail.com>
2022-02-13 17:22:55 +0530

    debugfalse

--- myproject/settings.py
+++ myproject/settings.py
@@ -121,16 +121,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
+# STATIC_ROOT = 'staticfiles'
 # STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = 'staticfiles'
+# MEDIA_ROOT = BASE_DIR/'media'
+# MEDIA_URL = '/media/'
+
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR/"static",
-# ]
-MEDIA_ROOT = BASE_DIR/'media'
+
 MEDIA_URL = '/media/'
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- myproject/urls.py
+++ myproject/urls.py
@@ -19,9 +19,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
+from django.urls import include, re_path
+
+from django.conf import settings
+
+from django.views.static import serve
+
 urlpatterns = [
     path('djangoadmin/', admin.site.urls),
     path('', include('mysite.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+
+  re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
