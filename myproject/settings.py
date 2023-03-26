d9e1acd7ced6ff3dde0870fd323509aedee3bc24
yash5015 <yashrathor5015@gmail.com>
2023-03-26 14:49:37 +0530

    try to deploy on railway

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
